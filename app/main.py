d652da5088c8b5532f5e91b2d4d873798136f4e6
Florian Ortner <FOrtner@inheco.com>
2022-02-11 17:26:20 +0100

    Lock/Unlock tests, PreConnect GetStatus/GetDeviceIdentification added

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 # IMPORTANT Path to IHC_PMS_Lib dlls
-sys.path.append("/home/for/dev/IHC_PMS_Lib_1.9.0.0/bin")
+sys.path.append("/home/for/dev/IHC_PMS_Lib_1.9.1.0/bin")
 from ihcWrappers import *
 
 
@@ -16,11 +16,13 @@ async def main():
     print("Configuration:")
     print("ODTC: " + config['IHC']['ODTC'])
     print("SCILA: " + config['IHC']['SCILA'])
-
+    print("PMS version: " + PmsWrapper.GetAssemblyVerion())
+    
     pms = PmsWrapper()
     pms.IpAddressIndex(1) #e.g. Linux VPN (multihomed adapter ip index)
 
     nics = pms.GetSupportedNICs() #default wired ethernet
+    #nics = pms.GetSupportedNICs(NetworkInterfaceTypesWrapper.Vpn) # Windows VPN
     if len(nics) == 0:
         nics = pms.GetSupportedNICs(NetworkInterfaceTypesWrapper.WiredEthernet + NetworkInterfaceTypesWrapper.VirtualEthernet)
     for nic in nics:
@@ -29,27 +31,42 @@ async def main():
     #nics = PmsWrapper.SupportedNICs(NetworkInterfaceTypesWrapper.WiredEthernet)
     
     if len(nics) == 1:
-        eru = pms.StartEventReceiver()
+        eru = pms.StartEventReceiver(nics[0].Id) # Explicit Id
     else:
         # manually choose an Id
-        eru = pms.StartEventReceiver(nics[0].Id)
+        eru = pms.StartEventReceiver(nics[1].Id)
     print("EventReceiver Uri: " + eru)
 
-    # status = PmsWrapper.GetStatus("10.2.2.5")
-    # status = PmsWrapper.GetStatus("10.2.2.8")
-
+    
     '''SCILA'''
     if config['IHC']['SCILA'] == "True":
+        print("SCILA Dll Version: " + ScilaWrapper.GetAssemblyVersion())
         # Finder
         frScilas = ScilaFinderWrapper.SearchDevices()
         if len(frScilas) == 0:
             print("No SCILA found")
         for val in frScilas:
-            print(val.Name + " " + str(val.WsdlUri))
-        # Create & register Callbacks
-        scila = pms.Create("http://10.2.2.5/scila.wsdl")
+            print("Finder result: " + val.Name + " " + str(val.WsdlUri))
+            # Gather device info before Create()
+            status = PmsWrapper.GetStatus(val.IPv4Address)
+            print(val.Name + " state: " + status.State)
+            print(val.Name + " locked: " + status.Locked)
+            (r, di) = PmsWrapper.GetDeviceIdentification(val.IPv4Address)
+            assert r.ReturnCode == 1
+            print(val.Name + " pre-connect GetDeviceIdentification: " + di.DeviceName + " " + di.Wsdl + " " + di.DeviceFirmwareVersion)
+
+        # Create device
+        try:
+            scila = pms.Create("http://10.2.2.5/scila.wsdl")
+        except Exception as ex:
+            if (ex.Message) == "Invalid lockId.":
+                scila = pms.Create("http://10.2.2.5/scila.wsdl", "myLockId")
+            else:
+                raise
+        # Register Callback(s)
         scila.RegisterStatusEventCallback(OnStatusEvent)
-        print(scila.DeviceName)
+        print("Device Name: " + scila.DeviceName)
+        print("Current LockId: " + str(scila.LockId))
         di = GetDeviceIdentification(scila)
         assert di.DeviceName == scila.DeviceName
         s = GetStatus(scila)
@@ -63,6 +80,23 @@ async def main():
         #     return
         s = GetStatus(scila)
         assert s.State == "Standby"
+        # Lock device
+        scila.LockId = "myLockId"
+        rv = scila.LockDevice()
+        assert rv.Success == True
+        # Test lock
+        s = GetStatus(scila)
+        assert s.Locked == True
+        scila.LockId = None # Set Wrong lockId
+        rv = scila.GetConfiguration()
+        assert rv.Success == False
+        # Unlock device
+        scila.LockId = "myLockId"
+        rv = scila.UnlockDevice()
+        assert rv.Success == True
+        s = GetStatus(scila)
+        assert s.Locked == False
+        # GetConfiguration
         rv = scila.GetConfiguration()
         assert rv.Success == True
         print(rv.ResponseData.LogLevel)
@@ -70,54 +104,67 @@ async def main():
         print(rv.ResponseData.SoapCompression)
         print(rv.ResponseData.SysDateTime)
         print(rv.ResponseData.UseDeviceClassDateTime)
-        # Configure Device: import Network-Settings from file
+        # Configure Device example: import Network-Settings from file
         cx = ScilaConfigXmlWrapper()
         cx.ImportNetworkConfigXml("dhcp.xml")
         print(cx.GetParamsXml())
+        # SetConfiguration
         rv = scila.SetConfiguration(cx.GetParamsXml())
         assert rv.Success == True
+        # Initialize
         rv = scila.Initialize()
         assert rv.Success == True
         s = GetStatus(scila)
         assert s.State == "Idle"
+        # Delay 1s
         rv = scila.Delay(1)
         assert rv.Success == True
+        # GetAutoBoostCo2
         rv = scila.GetAutoBoostCo2()
         assert rv.Success == True
         print(rv.ResponseData.AutoBoostCO2)
         print(rv.ResponseData.BoostState)
+        # GetCo2FlowStatus
         rv = scila.GetCo2FlowStatus()
         assert rv.Success == True
         print(rv.ResponseData.CO2FlowStatus)
+        # GetDoorStatus
         rv = scila.GetDoorStatus()
         assert rv.Success == True
         print(rv.ResponseData.Drawer1)
         print(rv.ResponseData.Drawer2)
         print(rv.ResponseData.Drawer3)
         print(rv.ResponseData.Drawer4)
+        # GetLiquidLevel
         rv = scila.GetLiquidLevel()
         assert rv.Success == True
         print(rv.ResponseData.LiquidLevel)
+        # GetParameters
         rv = scila.GetParameters()
         assert rv.Success == True
         print(rv.ResponseData.Position)
         print(rv.ResponseData.WorkstationMode)
+        # New Parameters config example
         px = ScilaParamsXmlWrapper()
         px.Position = 1
         px.WorkstationMode = False
         print(px.GetParamsXml())
+        # SetParameters
         rv = scila.SetParameters(px.GetParamsXml())
         assert rv.Success == True
+        # GetTemperature
         rv = scila.GetTemperature()
         assert rv.Success == True
         print(rv.ResponseData.CurrentTemperature)
         print(rv.ResponseData.TargetTemperature)
         print(rv.ResponseData.TemperatureControl)
+        # GetValveStatus
         rv = scila.GetValveStatus()
         assert rv.Success == True
         print(rv.ResponseData.Gas_Boost)
         print(rv.ResponseData.Gas_Normal)
         print(rv.ResponseData.H2O)
+        # Open-/CloseDoor
         rv = scila.PrepareForInput(4)
         assert rv.Success == True
         rv = scila.OpenDoor()
@@ -129,10 +176,13 @@ async def main():
         assert rv.Success == False
         # rv = scila.RetrieveByPositionId(2) #TODO Fix CommandException
         # assert rv.Success == True
+        # SetAutoBoostCo2
         rv = scila.SetAutoBoostCo2(True)
         assert rv.Success == True
+        # SetCo2NormalFlow
         rv = scila.SetCo2NormalFlow(True)
         assert rv.Success == True
+        # SetTemperature
         rv = scila.SetTemperature(37, True)
         assert rv.Success == True
         rv = scila.SetTemperature(None, True)
@@ -143,23 +193,41 @@ async def main():
         assert rv.Success == True
         # rv = scila.StoreAtPosition(2) #TODO Fix CommandException
         # assert rv.Success == True
+        # Disconnect
         scila.Dispose()
 
 
     '''ODTC'''
     if config['IHC']['ODTC'] == "True":
+        print("ODTC Dll Version: " + OdtcWrapper.GetAssemblyVersion())
         frOdtcs = OdtcFinderWrapper.SearchDevices()
         if len(frOdtcs) == 0:
             print("No ODTC found")
         for val in frOdtcs:
-            print(val.Name + " " + str(val.WsdlUri))
+            print("Finder result: " + val.Name + " " + str(val.WsdlUri))
+            # Gather device info before Create()
+            status = PmsWrapper.GetStatus(val.IPv4Address)
+            print(val.Name + " state: " + status.State)
+            print(val.Name + " locked: " + status.Locked)
+            (r, di) = PmsWrapper.GetDeviceIdentification(val.IPv4Address)
+            assert r.ReturnCode == 1
+            print("Pre-connect GetDeviceIdentification: " + di.DeviceName + " " + di.Wsdl + " " + di.DeviceFirmwareVersion)
         # with pms.Create("http://10.2.2.8/odtc.wsdl") as odtc:
         #     odtc.Reset()
         #     odtc.Initialize()
-        odtc = pms.Create("http://10.2.2.8/odtc.wsdl")
+        # Create device
+        try:
+            odtc = pms.Create("http://10.2.2.8/odtc.wsdl")
+        except Exception as ex:
+            if (ex.Message) == "Invalid lockId.":
+                odtc = pms.Create("http://10.2.2.8/odtc.wsdl", "myLockId")
+            else:
+                raise
+        # Register Callback(s)
         odtc.RegisterStatusEventCallback(OnStatusEvent)
         odtc.RegisterDataEventCallback(OnDataEvent)
-        print(odtc.DeviceName)
+        print("Device Name: " + odtc.DeviceName)
+        print("Current LockId: " + str(odtc.LockId))
         di = GetDeviceIdentification(odtc)
         assert di.DeviceName == odtc.DeviceName
         s = GetStatus(odtc)
@@ -173,6 +241,22 @@ async def main():
         #     exit()
         s = GetStatus(odtc)
         assert s.State == "Standby"
+        # Lock device
+        odtc.LockId = "myLockId"
+        rv = odtc.LockDevice()
+        assert rv.Success == True
+        # Test lock
+        s = GetStatus(odtc)
+        assert s.Locked == True
+        odtc.LockId = None # Set Wrong lockId
+        rv = odtc.GetConfiguration()
+        assert rv.Success == False
+        # Unlock device
+        odtc.LockId = "myLockId"
+        rv = odtc.UnlockDevice()
+        assert rv.Success == True
+        s = GetStatus(odtc)
+        assert s.Locked == False
         rv = odtc.GetConfiguration()
         assert rv.Success == True
         print(rv.ResponseData.LogLevel)
@@ -186,6 +270,8 @@ async def main():
         rv = odtc.SetConfiguration(cx.GetParamsXml())
         assert rv.Success == True
         rv = odtc.Initialize()
+        assert rv.Success == True
+        rv = odtc.StopMethod()
         assert rv.Success == True
         s = GetStatus(odtc)
         assert s.State == "Idle"
@@ -242,7 +328,8 @@ def GetStatus(device: DeviceWrapper) -> StatusWrapper:
         raise TypeError
     (r, s) = device.GetStatus()
     assert r.ReturnCode == 1
-    logging.debug(s.DeviceId + " " + s.CurrentTime + " " + str(s.Locked) + " " + s.PMSId + " " + s.State)
+    logging.debug(s.DeviceId + " " + s.CurrentTime + " " + str(s.Locked) + (" " + s.PMSId if s.PMSId else "") + " " + s.State)
+    #logging.debug(s.SubStates)
     return s
 
 def GetDeviceIdentification(device: DeviceWrapper) -> DeviceIdentificationWrapper:
